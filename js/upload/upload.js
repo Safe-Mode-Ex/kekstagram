59fd0c583904da2c819e84ba22e9842807ccbb21
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-06-18 17:26:21 +0300

    Вынесет валидацию в отдельный модуль

--- js/upload/upload.js
+++ js/upload/upload.js
@@ -1,65 +1,22 @@
 import { setupScale } from './scale';
 import { setupEffects } from './effects';
-
-const hashTagRegExp = /^#[a-zа-яё0-9]{1,19}$/i;
-const HASH_TAGS_MAX_COUNT = 5;
-const COMMENT_MAX_LENGHT = 140;
-
-const validationConfig = {
-  // class of the parent element where the error/success class is added
-  classTo: 'img-upload__field-wrapper',
-  // class of the parent element where error text element is appended
-  errorTextParent: 'img-upload__field-wrapper',
-  errorTextTag: 'p',
-  errorTextClass: 'img-upload__field-wrapper--error'
-};
+import { setupValidation } from './validate';
 
 const populateUploadImageCreator = (imgUploadInputEl, imgUploadOverlayEl, imgUploadFormEl) => {
   const imgUploadPreviewImageEl = imgUploadOverlayEl.querySelector('.img-upload__preview img');
 
   const {initializeScale, destroyScale} = setupScale(imgUploadOverlayEl, imgUploadPreviewImageEl);
   const {initializeEffects, destroyEffects} = setupEffects(imgUploadOverlayEl, imgUploadPreviewImageEl);
+  const {initializeValidation, hasErrors, destroyValidation} = setupValidation(imgUploadFormEl);
 
   const creator = () => {
     imgUploadPreviewImageEl.src = URL.createObjectURL(imgUploadInputEl.files[0]);
     initializeScale();
     initializeEffects();
-
-    const hashTagInputEl = imgUploadFormEl.querySelector('.text__hashtags');
-    const commentTextEl = imgUploadFormEl.querySelector('.text__description');
-    const pristine = new Pristine(imgUploadFormEl, validationConfig, false);
-
-    pristine.addValidator(hashTagInputEl, (value) => {
-      const hashTags = value.split(' ');
-      return !value || hashTags.every((hashTag) => hashTagRegExp.test(hashTag));
-    }, 'Введён невалидный хэштег');
-
-    pristine.addValidator(hashTagInputEl, (value) => {
-      const hashTags = value.split(' ');
-      return !value || hashTags.length <= HASH_TAGS_MAX_COUNT;
-    }, `Количество хэштегов не может быть больше ${HASH_TAGS_MAX_COUNT}`);
-
-    pristine.addValidator(hashTagInputEl, (value) => {
-      const hashTags = value.split(' ');
-      return !value || new Set(hashTags).size === hashTags.length;
-    }, 'Хэштеги повторяются');
-
-    pristine.addValidator(
-      commentTextEl,
-      (value) => !value.length || value.length <= COMMENT_MAX_LENGHT,
-      `Длина комментария не может быть больше ${COMMENT_MAX_LENGHT} символов`,
-    );
-
-    hashTagInputEl.addEventListener('change', () => {
-      pristine.validate();
-    });
-
-    commentTextEl.addEventListener('input', () => {
-      pristine.validate();
-    });
+    initializeValidation();
 
     imgUploadFormEl.addEventListener('submit', (evt) => {
-      if (pristine.getErrors()?.length) {
+      if (hasErrors()) {
         evt.preventDefault();
       }
     });
@@ -73,6 +30,7 @@ const populateUploadImageCreator = (imgUploadInputEl, imgUploadOverlayEl, imgUpl
 
     destroyScale();
     destroyEffects();
+    destroyValidation();
   };
 
   return creator;
---
+++ js/upload/validate.js
@@ -0,0 +1,56 @@
+import { COMMENT_MAX_LENGHT, HASH_TAG_REGEXP, HASH_TAGS_MAX_COUNT } from '../const';
+import { validationConfig } from './config';
+
+const setupValidation = (imgUploadFormEl) => {
+  const hashTagInputEl = imgUploadFormEl.querySelector('.text__hashtags');
+  const commentTextEl = imgUploadFormEl.querySelector('.text__description');
+  let validation;
+
+  const setValidators = (instance) => {
+    instance.addValidator(hashTagInputEl, (value) => {
+      const hashTags = value.split(' ');
+      return !value || hashTags.every((hashTag) => HASH_TAG_REGEXP.test(hashTag));
+    }, 'Введён невалидный хэштег');
+
+    instance.addValidator(hashTagInputEl, (value) => {
+      const hashTags = value.split(' ');
+      return !value || hashTags.length <= HASH_TAGS_MAX_COUNT;
+    }, `Количество хэштегов не может быть больше ${HASH_TAGS_MAX_COUNT}`);
+
+    instance.addValidator(hashTagInputEl, (value) => {
+      const hashTags = value.split(' ');
+      return !value || new Set(hashTags).size === hashTags.length;
+    }, 'Хэштеги повторяются');
+
+    instance.addValidator(
+      commentTextEl,
+      (value) => !value.length || value.length <= COMMENT_MAX_LENGHT,
+      `Длина комментария не может быть больше ${COMMENT_MAX_LENGHT} символов`,
+    );
+  };
+
+  const onHashTagChange = () => {
+    validation.validate();
+  };
+
+  const onCommentInput = () => {
+    validation.validate();
+  };
+
+  return {
+    initializeValidation: () => {
+      validation = new Pristine(imgUploadFormEl, validationConfig, false);
+      setValidators(validation);
+      hashTagInputEl.addEventListener('change', onHashTagChange);
+      commentTextEl.addEventListener('input', onCommentInput);
+    },
+    hasErrors: () => validation.getErrors()?.length,
+    destroyValidation: () => {
+      validation.destroy();
+      hashTagInputEl.removeEventListener('change', onHashTagChange);
+      commentTextEl.removeEventListener('input', onCommentInput);
+    },
+  };
+};
+
+export {setupValidation};
