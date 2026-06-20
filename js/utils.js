fb65d581491994974ad151d79adeafdf336269fc
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-06-20 20:57:21 +0300

    Добавит утилиты

--- js/utils.js
+++ js/utils.js
@@ -65,6 +65,29 @@ const notificationState = {
   isOpen: false,
 };
 
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+
 export {
   getRandomInteger,
   getRandomArrayElement,
@@ -77,4 +100,6 @@ export {
   setElementVisibility,
   cleanError,
   notificationState,
+  debounce,
+  throttle,
 };
